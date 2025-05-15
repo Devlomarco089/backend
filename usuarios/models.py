d535487425751b98c96c9a36bd914181efd6a2e2
Devlomarco089 <rafaocampoel8765@gmail.com>
2025-05-15 20:06:21 -0300

    solucion crear usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,11 +1,38 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El correo electrónico es obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('El superusuario debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('El superusuario debe tener is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Correo Electrónico')
-    username = models.CharField(max_length=150, blank=True, null=True)  # Hacer opcional
+    first_name = models.CharField(max_length=150, verbose_name='Nombre')
+    last_name = models.CharField(max_length=150, verbose_name='Apellido')
+    username = models.CharField(max_length=150, blank=True, null=True)  # Opcional
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'  # Identificador para iniciar sesión
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # Campos obligatorios al crear un usuario
+
+    objects = CustomUserManager()  # Asigna el nuevo UserManager
+
     def __str__(self):
         return self.email
--- usuarios/serializer.py
+++ usuarios/serializer.py
@@ -4,16 +4,19 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
-    username = serializers.CharField(required=False, allow_blank=True)  # Hacer opcional
+    first_name = serializers.CharField(required=True)  # Obligatorio
+    last_name = serializers.CharField(required=True)  # Obligatorio
 
     class Meta:
         model = CustomUser
-        fields = ('id', 'email', 'username', 'password')
+        fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name')
 
     def create(self, validated_data):
         user = CustomUser(
             email=validated_data['email'],
-            username=validated_data.get('username', '')  # Asignar un valor predeterminado si no se proporciona
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            username=validated_data.get('username', '')  # Opcional
         )
         user.set_password(validated_data['password'])
         user.save()
