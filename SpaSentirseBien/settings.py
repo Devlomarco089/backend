672ca22992cc9e35d42cf3c749af7cfcd48184a6
Devlomarco089 <rafaocampoel8765@gmail.com>
2025-05-15 23:00:03 -0300

    settings

--- SpaSentirseBien/settings.py
+++ SpaSentirseBien/settings.py
@@ -191,8 +191,7 @@ STATIC_URL = '/static/'
 
 CSRF_TRUSTED_ORIGINS = ['https://*','https://web-production-5825.up.railway.app']
 
-MEDIA_URL = '/media/'  # URL base para acceder a los archivos de medios
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Carpeta donde se almacenan los
+
 
 
 STATICFILES_DIRS = [
